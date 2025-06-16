d96bcfa8664c7f51798bfc714fb85fbae3ac8cce
FleaB <51238114+FleaB@users.noreply.github.com>
2025-06-16 18:43:05 +0200

    Shaking & Tilting + Board Fixes

--- Assets/Scripts/PlaceEgg.cs
+++ Assets/Scripts/PlaceEgg.cs
@@ -3,6 +3,7 @@ using DG.Tweening;
 
 public class PlaceEgg : MonoBehaviour
 {
+    [SerializeField] Transform plinkoMachine;
     [SerializeField] GameObject egg;
     [SerializeField] Vector3[] path;
     [SerializeField] float pathDuration;
@@ -11,6 +12,7 @@ public class PlaceEgg : MonoBehaviour
     private Rigidbody2D eggRB;
     bool egging;
     bool placedEgg;
+    bool shaking;
     void Awake()
     {
         if (!egging)
@@ -24,17 +26,52 @@ public class PlaceEgg : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("SPACE");
             if (!egging)
             {
                 Debug.Log("NewEgg");
                 NewEgg();
             }
-            else if(!placedEgg)
+            else if (!placedEgg)
             {
                 Debug.Log("PlaceEgg");
                 EggPlace();
             }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            if (placedEgg)
+            {
+                Debug.Log("SHAKING");
+                plinkoMachine.DOShakeRotation(1, 1, 20, 20, true);
+                shaking = true;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (shaking)
+            {
+                plinkoMachine.DORotate(Vector3.zero, 1);
+                shaking = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            plinkoMachine.DORotate(new Vector3(0, 0, 10), 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            plinkoMachine.DORotate(new Vector3(0, 0, -10), 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            plinkoMachine.DORotate(Vector3.zero, 1);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            plinkoMachine.DORotate(Vector3.zero, 1);
         }
     }
 
